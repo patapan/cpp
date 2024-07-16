4a95e9dc31edc9e6c4cd79b3a5b3438bf6c8c1d1
patapan <alexpatapan@gmail.com>
2024-07-16 12:41:42 +1000

    add prime example

--- src/datastructures/malloc.cpp
+++ src/datastructures/malloc.cpp
@@ -1,10 +1,49 @@
+#include <utility>
 
+template <typename T>
+class UniquePointer {
+ private:
+    T* data;
 
-void run() {
-    int* var = new int[99999990000000000];
-    delete[] var;
+ public:
+    UniquePointer(T* data = nullptr) : data(data) {}
+
+    ~UniquePointer() {
+        delete data;
+    }
+
+    T* get() {
+        return data;
+    }
+
+    // delete copy constructor & copy assignment
+    // because unique pointers are *unique*
+    UniquePointer(const UniquePointer& other) = delete;
+    UniquePointer& operator=(const UniquePointer& other) = delete;
+
+    // move constructor
+    UniquePointer(UniquePointer&& other) noexcept {
+        delete data;
+        data = other.data;
+        other.data = nullptr;
+    }
+
+    // move assignment 
+    UniquePointer& operator=(UniquePointer&& other) noexcept {
+        if (this != &other) {
+            delete data;
+            data = other.data;
+            other.data = nullptr;
+        }
+        return *this;
+    }
+};
+
+// make_unique constructs an object and returns a unique pointer to it. this way we 
+// know for certain that this pointer is unique.
+template <typename T, typename ...Args>
+UniquePointer<T> make_unique(Args&& ...args) {
+    return UniquePointer<T>(new T(std::forward(args)...));
 }
 
-int main() {
-    run();
-}
+int main(){}
--- src/template_metaprogramming/prime.cc
+++ src/template_metaprogramming/prime.cc
@@ -2,12 +2,52 @@
 // Check if a number is prime at compile time
 // https://www.youtube.com/watch?v=cP1r8BonnuA&pp=ygUsdGVtcGxhdGUgbWV0YXByb2dyYW1taW5nIGludGVydmlldyBxdWVzdGlvbnM%3D
 
-// 
-constexpr bool is_prime(int i) {
-    return is_prime()
+#include <type_traits>
+
+namespace metaprogramming {
+    template <int N, int M>
+    struct is_prime_helper {
+        static constexpr bool value = is_prime_helper<N,M-1>::value && (N%M != 0);
+    };
+
+    template <int N>
+    struct is_prime_helper<N,1> {
+        static constexpr bool value = true;
+    };
+
+    template <int T, typename = void>
+    struct is_prime {
+        static constexpr bool value = is_prime_helper<T,T-1>::value;
+    };
+
+    template <int N>
+    struct is_prime<N, std::enable_if_t<(N < 2)>> {
+        constexpr bool static value = false;
+    };
+
+}
+
+namespace normalway {
+    consteval bool is_prime(int i, int j) {
+        if (j == 1) return true;
+        if (i%j == 0) return false;
+        return is_prime(i,j-1);
+    }
+
+    // First we do it the 'normal' way.
+    consteval bool is_prime(int i) {
+        if (i <= 1) return false;
+        return is_prime(i,i-1);
+    }
 }
 
 
 int main() {
-    std::static_assert<>
+    static_assert(normalway::is_prime(13),"");
+    static_assert(!normalway::is_prime(10),"");
+
+    static_assert(!metaprogramming::is_prime<1>::value, "");
+    static_assert(!metaprogramming::is_prime<10>::value, "");
+    static_assert(metaprogramming::is_prime<13>::value, "");
+    
 }
