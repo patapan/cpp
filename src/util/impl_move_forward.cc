cc8f88cc2a9ab5b71bcba216ad99d923e124ef57
patapan <alexpatapan@gmail.com>
2024-08-10 13:06:16 +1000

    impl std move and std forward

---
+++ src/util/impl_move_forward.cc
@@ -0,0 +1,78 @@
+/*
+std forward retains the ref type of the value that is passed in.
+
+The point of this is to invoke the correct constructor for a given class (copy, move)
+*/
+
+#include <utility> 
+#include <memory>
+#include <string>
+#include <iostream>
+
+namespace alex {
+
+template <typename T>
+struct remove_reference { 
+    using type = T;
+};
+
+template <typename T>
+struct remove_reference<T&&> {
+    using type = T;
+};
+
+template <typename T>
+struct remove_reference<T&> {
+    using type = T;
+};
+
+// move is a casting operator from L value to R value
+template <typename T>
+constexpr remove_reference<T>::type&& move(T&& t) {
+    return static_cast<remove_reference<T>::type&&>(t);
+}
+
+// forward retains the value type of the arg passed in.
+template <typename T>
+constexpr T&& forward(typename std::remove_reference<T>::type& t) {
+    return static_cast<T&&>(t);
+}
+
+template <typename T>
+constexpr T&& forward(typename std::remove_reference<T>::type&& t) {
+    return static_cast<T&&>(t);
+}
+
+class Factory {
+private:
+    std::string name;
+public:
+    // move 
+    Factory(std::string&& name) noexcept : name(alex::move(name)){
+        std::cout << "Move constructor\n";
+    }
+
+    // copy
+    Factory(const std::string& name) : name(name){
+        std::cout << "Copy constructor\n";
+    } 
+};
+
+
+template <typename T, typename ...Args>
+std::unique_ptr<T> make_unique(Args&&... args) {
+    return std::unique_ptr<T>(new Factory(std::forward<Args>(args)...));
+}
+
+}  // namespace alex
+
+int main() {
+    auto p = alex::make_unique<alex::Factory>("alex");
+
+    std::string name = "stephen";
+
+    auto p2 = alex::make_unique<alex::Factory>(name);
+    auto p3 = alex::make_unique<alex::Factory>(alex::move(name));
+
+    std::cout << name << std::endl;
+}
