07ef4359b824b0cfb865c49398ef7a423fec3f05
patapan <alexpatapan@gmail.com>
2024-09-12 08:16:45 +1000

    add optimized quicksort

--- src/algorithms/sort/quick_select.cc
+++ src/algorithms/sort/quick_select.cc
@@ -3,6 +3,9 @@
 // Space complexity: O(n)
 class Solution {
 public:
+    // This solution should be opimitzed to be O(1) space
+    //  Other implementations of Quickselect can avoid creating these three in memory, 
+    // but in the worst-case scenario, those implementations would still require O(n) space for the recursion call stack.
     int findKthLargest(vector<int>& nums, int k) {
         int pivot = nums[rand() % nums.size()];
 
@@ -32,6 +35,39 @@ public:
         }
 
         return pivot;
+    }
+
+private:
+    // Optimized quickselect with O(N) time and O(1) average case space.
+
+    int partition(vector<int>& nums, int left, int right) {
+        int pivotIndex = left + (right - left) / 2;
+        int pivotValue = nums[pivotIndex];
+        swap(nums[pivotIndex], nums[right]);  // Move pivot to end for ease
+        int storeIndex = left;
+        for (int i = left; i < right; i++) {
+            if (nums[i] < pivotValue) {
+                swap(nums[i], nums[storeIndex]);
+                storeIndex++;
+            }
+        }
+        swap(nums[storeIndex], nums[right]);  // Move pivot to its final place
+        return storeIndex;
+    }
+
+    int quickselect(vector<int>& nums, int left, int right, int k) {
+        if (left == right) return nums[left];  // If the list contains only one element,
+
+        int pivotIndex = partition(nums, left, right);
+
+        // The pivot is in its final sorted position
+        if (k == pivotIndex) {
+            return nums[k];
+        } else if (k < pivotIndex) {
+            return quickselect(nums, left, pivotIndex - 1, k);
+        } else {
+            return quickselect(nums, pivotIndex + 1, right, k);
+        }
     }
 
 };
